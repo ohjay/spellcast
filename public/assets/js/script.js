4b16e135094968b52445049588e97025cb698b13
Owen Jow <owenjow@berkeley.edu>
2018-04-22 14:56:54 -0700

    Load test Babylon.js scene

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,7 +1,47 @@
-var isWand = true;
+import * as BABYLON from 'babylonjs';
 
 function logInfo(msg) {
   document.getElementById('info').textContent += msg + ' ';
+}
+
+function loadScene() {
+  var canvas = document.getElementById('renderCanvas');
+  var engine = new BABYLON.Engine(canvas, true);
+  var createScene = function() {
+    // Create a basic BJS Scene object.
+    var scene = new BABYLON.Scene(engine);
+
+    // Create a FreeCamera, and set its position to (x: 0, y: 5, z: -10).
+    var camera = new BABYLON.FreeCamera('camera', new BABYLON.Vector3(0, 5,-10), scene);
+
+    // Target the camera to scene origin.
+    camera.setTarget(BABYLON.Vector3.Zero());
+
+    // Attach the camera to the canvas.
+    camera.attachControl(canvas, false);
+
+    // Create a basic light, aiming at 0, 1, 0 - meaning, to the sky.
+    var light = new BABYLON.HemisphericLight('light1', new BABYLON.Vector3(0,1,0), scene);
+
+    // Create a built-in "sphere" shape.
+    var sphere = BABYLON.MeshBuilder.CreateSphere('sphere', {segments: 16, diameter: 2}, scene);
+
+    // Move the sphere upward 1/2 of its height.
+    sphere.position.y = 1;
+
+    // Create a built-in "ground" shape.
+    var ground = BABYLON.MeshBuilder.CreateGround('ground1', {height:6, width:6, subdivisions: 2}, scene);
+
+    // Return the created scene.
+    return scene;
+  }
+  var scene = createScene();
+  engine.runRenderLoop(function() {
+    scene.render();
+  });
+  window.addEventListener('resize', function() {
+    engine.resize();
+  });
 }
 
 $(function() {
@@ -33,10 +73,10 @@ $(function() {
   function handleMotion(evt) {
     var acceleration = evt.acceleration; // contains accel.x, accel.y, accel.z (m/s^2)
     if (acceleration.x == null) {
-      logInfo('Acceleration is invalid.');
-      logInfo('Falling back to acceleration including gravity: ' + JSON.stringify(evt.accelerationIncludingGravity, null, 4));
       acceleration = evt.accelerationIncludingGravity;
-      acceleration.z -= 9.81;
+      if (acceleration.z != null) {
+        acceleration.z -= 9.81;
+      }
     }
     var rotationRate = evt.rotationRate; // rotation rate around each axis (deg/s)
     var interval = evt.interval; // ms time interval at which data is obtained from device
@@ -58,7 +98,6 @@ $(function() {
     } else {
       logInfo('Error: device motion not supported!');
     }
-    isWand = true;
   };
 
   /*
@@ -79,4 +118,8 @@ $(function() {
     document.getElementById('paccely').textContent = 'acceleration y: ' + v.y.toString();
     document.getElementById('paccelz').textContent = 'acceleration z: ' + v.z.toString();
   });
+
+  document.getElementById('scene').onclick = function() {
+    loadScene();
+  };
 });
